386d8f4e1066a61a7c8db5887d6f74bc4ab7b693
CodeBySiam <codebysiam@gmail.com>
2026-07-26 01:58:08 +0600

    new:arrays Finding Max/Min Subarray using Kadane's Algorithm in O(n) time

---
+++ Algorithms/arrays/finding_max_min_subarray_kadanes_algo.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+/** --------------------- Finding Max/Min Subarray (Kadane's Algorithm) ----------------------- */
+/*
+It is simple to find the max/min subarray in O(n^3) or O(n^2)
+but we can use Kadane's Algorithm to find it in O(n) time complexity
+*/
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(auto&x : a){
+        cin >> x;
+    }
+    ll mx = a[0];
+    ll cur = a[0];
+    int curL = 0;
+    int bestL = 0;
+    int bestR = 0;
+    for(int i = 1; i<n; i++){
+        if(cur < 0){
+            cur = a[i];
+            curL = i;
+        }else{
+            cur+=a[i];
+        }
+
+        if(cur > mx){
+            mx = cur;
+            bestL = curL;
+            bestR = i;
+        }
+    }
+
+    cout << mx;
+    cout << '\n';
+    for(int i = bestL; i<=bestR; i++){
+        cout << a[i] << ' ';
+    }
+    return 0;
+}
